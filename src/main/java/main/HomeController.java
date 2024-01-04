edf5933675de8d69efcf77add24e5b6c232b5413
CraftTex <nadinezuhdan@gmail.com>
2024-01-05 00:09:31 +0700

    comparator is now a lambda expression

--- src/main/java/main/HomeController.java
+++ src/main/java/main/HomeController.java
@@ -49,13 +49,10 @@ public class HomeController {
     public static List<Song> recommendation(int limit) {
         List<Song> allSong = SongDAO.getAllSong();
         Date currDate = new Date();
-        allSong.sort(new Comparator<Song>() {
-            @Override
-            public int compare(Song o1, Song o2) {
-                long v1 = (long) ((Math.pow(o1.getCountPlayed(), 2) * 10) - (currDate.getTime() / 1000 - o1.getLastPlayed().getTime() / 1000));
-                long v2 = (long) ((Math.pow(o2.getCountPlayed(), 2) * 10) - (currDate.getTime() / 1000 - o2.getLastPlayed().getTime() / 1000));
-                return (int) (v2 - v1);
-            }
+        allSong.sort((o1, o2) -> {
+            long v1 = (long) ((Math.pow(o1.getCountPlayed(), 2) * 10) - (currDate.getTime() / 1000 - o1.getLastPlayed().getTime() / 1000));
+            long v2 = (long) ((Math.pow(o2.getCountPlayed(), 2) * 10) - (currDate.getTime() / 1000 - o2.getLastPlayed().getTime() / 1000));
+            return (int) (v2 - v1);
         });
         return allSong.subList(0, limit);
     }
